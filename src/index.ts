816d7aa8347c42e19708b19128d2b8fca029801e
Frugal-Wizard <108354301+Frugal-Wizard@users.noreply.github.com>
2022-11-11 14:33:16 -0300

    update deps

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,16 @@ if (require.main === module) {
     void (async function() {
         console.log('Starting server...');
         console.log();
-        const { addressBook, logicRegistry, operatorFactory, operatorLogic, orderbookFactory, tokens, orderbooks } = await startServer({
+        const { addressBook, operatorFactory, operatorV1, orderbookFactory, tokens, orderbooks } = await startServer({
             dbPath: path.resolve(process.cwd(), 'db'),
             verbose: process.argv.includes('--verbose'),
         });
         console.log('AddressBook address:');
         console.log(`    ${addressBook}`);
-        console.log('OperatorLogicRegistry address:');
-        console.log(`    ${logicRegistry}`);
         console.log('OperatorFactory address:');
         console.log(`    ${operatorFactory}`);
-        console.log('OperatorLogicV1 address:');
-        console.log(`    ${operatorLogic}`);
+        console.log('OperatorV1 address:');
+        console.log(`    ${operatorV1}`);
         console.log('OrderbookFactoryV1 address:');
         console.log(`    ${orderbookFactory}`);
         console.log('Tokens addresses:');
--- src/test-server.ts
+++ src/test-server.ts
@@ -1,12 +1,11 @@
 import ganache, { EthereumProvider } from 'ganache';
 import { AddressBook } from '@frugal-wizard/addressbook/dist/AddressBook';
 import { ERC20Mock } from '@theorderbookdex/orderbook-dex/dist/testing/ERC20Mock';
-import { OperatorLogicRegistry } from '@theorderbookdex/orderbook-dex-operator/dist/OperatorLogicRegistry';
 import { OperatorFactory } from '@theorderbookdex/orderbook-dex-operator/dist/OperatorFactory';
-import { OperatorLogicV1 } from '@theorderbookdex/orderbook-dex-v1/dist/OperatorLogicV1';
 import { OrderbookCreated, OrderbookFactoryV1 } from '@theorderbookdex/orderbook-dex-v1/dist/OrderbookFactoryV1';
 import { startOrderbookSimulation } from './orderbook-simulation';
 import { createSigner, getBlockNumber, getContractAddress, hexstring } from '@frugal-wizard/abi2ts-lib';
+import { OperatorV1 } from '@theorderbookdex/orderbook-dex-v1-operator/dist/OperatorV1';
 
 export interface ServerOptions {
     dbPath?: string,
@@ -16,9 +15,8 @@ export interface ServerOptions {
 
 export interface Server {
     addressBook: string,
-    logicRegistry: string;
     operatorFactory: string;
-    operatorLogic: string;
+    operatorV1: string;
     orderbookFactory: string,
     tokens: { [symbol: string]: string },
     orderbooks: { [pair: string]: string },
@@ -44,9 +42,8 @@ export async function startServer({ dbPath, port = 8545, verbose }: ServerOption
         const signer = await createSigner('0x0000000000000000000000000000000000000000000000000000000000000001');
         await global.ethereum.send('evm_setAccountBalance', [ signer.address, hexstring(1000000000000000000000n) ]);
         const addressBook      = (await signer.sendTransaction(await AddressBook.populateTransaction.deploy())).contractAddress;
-        const logicRegistry    = (await signer.sendTransaction(await OperatorLogicRegistry.populateTransaction.deploy())).contractAddress;
-                                 (await signer.sendTransaction(await OperatorFactory.populateTransaction.deploy(logicRegistry, addressBook))).contractAddress;
-        const operatorLogic    = (await signer.sendTransaction(await OperatorLogicV1.populateTransaction.deploy())).contractAddress;
+        const operatorFactory  = (await signer.sendTransaction(await OperatorFactory.populateTransaction.deploy(signer.address, addressBook))).contractAddress;
+        const operatorV1       = (await signer.sendTransaction(await OperatorV1.populateTransaction.deploy())).contractAddress;
         const orderbookFactory = (await signer.sendTransaction(await OrderbookFactoryV1.populateTransaction.deploy(addressBook))).contractAddress;
         const WBTC             = (await signer.sendTransaction(await ERC20Mock.populateTransaction.deploy('Wrapped BTC', 'WBTC', 18))).contractAddress;
         const WETH             = (await signer.sendTransaction(await ERC20Mock.populateTransaction.deploy('Wrapped Ether', 'WETH', 18))).contractAddress;
@@ -57,13 +54,12 @@ export async function startServer({ dbPath, port = 8545, verbose }: ServerOption
         await signer.sendTransaction(await OrderbookFactoryV1.at(orderbookFactory).populateTransaction.createOrderbook(WETH, USDT, 10000000000000000n,  100000n));
         await signer.sendTransaction(await OrderbookFactoryV1.at(orderbookFactory).populateTransaction.createOrderbook(BNB,  USDT, 100000000000000000n, 100000n));
         await signer.sendTransaction(await OrderbookFactoryV1.at(orderbookFactory).populateTransaction.createOrderbook(WXRP, USDT, 1000000000000000000n, 10000n));
-        await signer.sendTransaction(await OperatorLogicRegistry.at(logicRegistry).populateTransaction.register(10000n, operatorLogic));
+        await signer.sendTransaction(await OperatorFactory.at(operatorFactory).populateTransaction.registerVersion(10000n, operatorV1));
     }
     let blockNumber = 2;
     const addressBook      = await getContractAddress(blockNumber++, 0);
-    const logicRegistry    = await getContractAddress(blockNumber++, 0);
     const operatorFactory  = await getContractAddress(blockNumber++, 0);
-    const operatorLogic    = await getContractAddress(blockNumber++, 0);
+    const operatorV1       = await getContractAddress(blockNumber++, 0);
     const orderbookFactory = await getContractAddress(blockNumber++, 0);
     const WBTC             = await getContractAddress(blockNumber++, 0);
     const WETH             = await getContractAddress(blockNumber++, 0);
@@ -78,5 +74,5 @@ export async function startServer({ dbPath, port = 8545, verbose }: ServerOption
         startOrderbookSimulation(orderbook, verbose);
     }
     await server.listen(port);
-    return { addressBook, logicRegistry, operatorFactory, operatorLogic, orderbookFactory, tokens, orderbooks };
+    return { addressBook, operatorFactory, operatorV1, orderbookFactory, tokens, orderbooks };
 }
