0f5f7c90c4a9fcfb3b711d639fc5dbdd578174ab
Frugal-Wizard <108354301+Frugal-Wizard@users.noreply.github.com>
2022-11-21 13:51:10 -0300

    use ERC20WithFaucet instead of ERC20Mock

--- src/orderbook-simulation.ts
+++ src/orderbook-simulation.ts
@@ -2,7 +2,7 @@ import { createSigner, formatValue, getBalance, hexstring, hexstringPad } from '
 import { IAddressBook } from '@frugal-wizard/addressbook/dist/interfaces/IAddressBook';
 import { EthereumProvider } from 'ganache';
 import { IOrderbookV1 } from '@theorderbookdex/orderbook-dex-v1/dist/interfaces/IOrderbookV1';
-import { IERC20Mock } from '@theorderbookdex/orderbook-dex/dist/testing/interfaces/IERC20Mock';
+import { IERC20WithFaucet } from '@theorderbookdex/orderbook-dex/dist/testing/interfaces/IERC20WithFaucet';
 
 const SIMULATION_DELAY = 10000;
 
@@ -20,69 +20,91 @@ const global = globalThis as Global;
 export function startOrderbookSimulation(address: string, verbose = false) {
     void (async function() {
         const signer = await createSigner(hexstringPad(address, 64));
+
         if (!await getBalance(signer.address)) {
             await global.ethereum?.send('evm_setAccountBalance', [ signer.address, hexstring(1000000000000000000000n) ]);
         }
+
         const orderbook = IOrderbookV1.at(address);
-        const tradedToken = IERC20Mock.at(await orderbook.tradedToken());
-        const baseToken = IERC20Mock.at(await orderbook.baseToken());
+        const tradedToken = IERC20WithFaucet.at(await orderbook.tradedToken());
+        const baseToken = IERC20WithFaucet.at(await orderbook.baseToken());
         const contractSize = await orderbook.contractSize();
         const priceTick = await orderbook.priceTick();
         const pair = `${await tradedToken.symbol()}/${await baseToken.symbol()}`;
         const decimals = await baseToken.decimals();
+
         {
             const addressBook = IAddressBook.at(await orderbook.addressBook());
             if (!await addressBook.id(signer.address)) {
                 await signer.sendTransaction(await addressBook.populateTransaction.register());
             }
         }
+
+        try {
+            await signer.sendTransaction(await tradedToken.populateTransaction.faucet());
+        } catch {
+            // ignore
+        }
+
+        try {
+            await signer.sendTransaction(await baseToken.populateTransaction.faucet());
+        } catch {
+            // ignore
+        }
+
         let bullish = true;
+
         // eslint-disable-next-line no-constant-condition
         while (true) {
             try {
                 const askPrice = await orderbook.askPrice();
                 const bidPrice = await orderbook.bidPrice();
+
                 if (randomBoolean()) {
                     // BUY
                     const price = randomPrice(askPrice || bidPrice || 100n * priceTick, priceTick, bullish);
+
                     if (!askPrice || price < askPrice) {
                         // PLACE
                         const amount = BigInt(randomInt(10) + 1);
                         if (verbose) console.log(`Placing buy ${amount} contract(s) order at ${formatValue(price, decimals)} in ${pair}`);
-                        await signer.sendTransaction(await baseToken.populateTransaction.giveMe(amount * price));
                         await signer.sendTransaction(await baseToken.populateTransaction.approve(orderbook, amount * price));
                         await signer.sendTransaction(await orderbook.populateTransaction.placeOrder(OrderType.BUY, price, amount));
+
                     } else {
                         // FILL
                         const amount = BigInt(randomInt(5) + 1);
                         if (verbose) console.log(`Buying ${amount} contract(s) at ${formatValue(price, decimals)} or better in ${pair}`);
-                        await signer.sendTransaction(await baseToken.populateTransaction.giveMe(amount * price));
                         await signer.sendTransaction(await baseToken.populateTransaction.approve(orderbook, amount * price));
                         await signer.sendTransaction(await orderbook.populateTransaction.fill(OrderType.SELL, amount, price, 255));
                     }
+
                 } else {
                     // SELL
                     const price = randomPrice(bidPrice || askPrice || 100n * priceTick, priceTick, bullish);
+
                     if (!bidPrice || price > bidPrice) {
                         // PLACE
                         const amount = BigInt(randomInt(10) + 1);
                         if (verbose) console.log(`Placing sell ${amount} contract(s) order at ${formatValue(price, decimals)} in ${pair}`);
-                        await signer.sendTransaction(await tradedToken.populateTransaction.giveMe(amount * contractSize));
                         await signer.sendTransaction(await tradedToken.populateTransaction.approve(orderbook, amount * contractSize));
                         await signer.sendTransaction(await orderbook.populateTransaction.placeOrder(OrderType.SELL, price, amount));
+
                     } else {
                         // FILL
                         const amount = BigInt(randomInt(5) + 1);
                         if (verbose) console.log(`Selling ${amount} contract(s) at ${formatValue(price, decimals)} or better in ${pair}`);
-                        await signer.sendTransaction(await tradedToken.populateTransaction.giveMe(amount * contractSize));
                         await signer.sendTransaction(await tradedToken.populateTransaction.approve(orderbook, amount * contractSize));
                         await signer.sendTransaction(await orderbook.populateTransaction.fill(OrderType.BUY, amount, price, 255));
                     }
                 }
+
             } catch (error) {
                 console.error(error);
             }
+
             if (!randomInt(10)) bullish = !bullish;
+
             await wait(SIMULATION_DELAY);
         }
     })();
--- src/test-server.ts
+++ src/test-server.ts
@@ -6,6 +6,7 @@ import { OrderbookCreated, OrderbookFactoryV1 } from '@theorderbookdex/orderbook
 import { startOrderbookSimulation } from './orderbook-simulation';
 import { createSigner, getBlockNumber, getContractAddress, hexstring } from '@frugal-wizard/abi2ts-lib';
 import { OperatorV1 } from '@theorderbookdex/orderbook-dex-v1-operator/dist/OperatorV1';
+import { ERC20WithFaucet } from '@theorderbookdex/orderbook-dex/dist/testing/ERC20WithFaucet';
 
 export interface ServerOptions {
     dbPath?: string,
@@ -28,6 +29,8 @@ interface Global {
 
 const global = globalThis as Global;
 
+const ONE_DAY = 24n * 60n * 60n;
+
 export async function startServer({ dbPath, port = 8545, verbose }: ServerOptions): Promise<Server> {
     const server = ganache.server({
         logging: {
@@ -37,42 +40,56 @@ export async function startServer({ dbPath, port = 8545, verbose }: ServerOption
             dbPath,
         },
     });
+
     global.ethereum = server.provider;
+
     if (await getBlockNumber() == 0) {
         const signer = await createSigner('0x0000000000000000000000000000000000000000000000000000000000000001');
         await global.ethereum.send('evm_setAccountBalance', [ signer.address, hexstring(1000000000000000000000n) ]);
+
         const addressBook      = (await signer.sendTransaction(await AddressBook.populateTransaction.deploy())).contractAddress;
         const operatorFactory  = (await signer.sendTransaction(await OperatorFactory.populateTransaction.deploy(signer.address, addressBook))).contractAddress;
         const operatorV1       = (await signer.sendTransaction(await OperatorV1.populateTransaction.deploy())).contractAddress;
         const orderbookFactory = (await signer.sendTransaction(await OrderbookFactoryV1.populateTransaction.deploy(addressBook))).contractAddress;
-        const WBTC             = (await signer.sendTransaction(await ERC20Mock.populateTransaction.deploy('Wrapped BTC', 'WBTC', 18))).contractAddress;
-        const WETH             = (await signer.sendTransaction(await ERC20Mock.populateTransaction.deploy('Wrapped Ether', 'WETH', 18))).contractAddress;
-        const BNB              = (await signer.sendTransaction(await ERC20Mock.populateTransaction.deploy('BNB', 'BNB', 18))).contractAddress;
-        const WXRP             = (await signer.sendTransaction(await ERC20Mock.populateTransaction.deploy('Wrapped XRP', 'WXRP', 18))).contractAddress;
-        const USDT             = (await signer.sendTransaction(await ERC20Mock.populateTransaction.deploy('Tether USD', 'USDT', 6))).contractAddress;
+
+        const WBTC = (await signer.sendTransaction(await ERC20WithFaucet.populateTransaction.deploy('Wrapped BTC',   'WBTC', 18,    1000000000000000000000n, ONE_DAY))).contractAddress;
+        const WETH = (await signer.sendTransaction(await ERC20WithFaucet.populateTransaction.deploy('Wrapped Ether', 'WETH', 18,   10000000000000000000000n, ONE_DAY))).contractAddress;
+        const BNB  = (await signer.sendTransaction(await ERC20WithFaucet.populateTransaction.deploy('BNB',           'BNB',  18,  100000000000000000000000n, ONE_DAY))).contractAddress;
+        const WXRP = (await signer.sendTransaction(await ERC20WithFaucet.populateTransaction.deploy('Wrapped XRP',   'WXRP', 18, 1000000000000000000000000n, ONE_DAY))).contractAddress;
+        const USDT = (await signer.sendTransaction(await ERC20WithFaucet.populateTransaction.deploy('Tether USD',    'USDT',  6,             1000000000000n, ONE_DAY))).contractAddress;
+
         await signer.sendTransaction(await OrderbookFactoryV1.at(orderbookFactory).populateTransaction.createOrderbook(WBTC, USDT, 1000000000000000n,   100000n));
         await signer.sendTransaction(await OrderbookFactoryV1.at(orderbookFactory).populateTransaction.createOrderbook(WETH, USDT, 10000000000000000n,  100000n));
         await signer.sendTransaction(await OrderbookFactoryV1.at(orderbookFactory).populateTransaction.createOrderbook(BNB,  USDT, 100000000000000000n, 100000n));
         await signer.sendTransaction(await OrderbookFactoryV1.at(orderbookFactory).populateTransaction.createOrderbook(WXRP, USDT, 1000000000000000000n, 10000n));
+
         await signer.sendTransaction(await OperatorFactory.at(operatorFactory).populateTransaction.registerVersion(10000n, operatorV1));
     }
+
     let blockNumber = 2;
+
     const addressBook      = await getContractAddress(blockNumber++, 0);
     const operatorFactory  = await getContractAddress(blockNumber++, 0);
     const operatorV1       = await getContractAddress(blockNumber++, 0);
     const orderbookFactory = await getContractAddress(blockNumber++, 0);
-    const WBTC             = await getContractAddress(blockNumber++, 0);
-    const WETH             = await getContractAddress(blockNumber++, 0);
-    const BNB              = await getContractAddress(blockNumber++, 0);
-    const WXRP             = await getContractAddress(blockNumber++, 0);
-    const USDT             = await getContractAddress(blockNumber++, 0);
+
+    const WBTC = await getContractAddress(blockNumber++, 0);
+    const WETH = await getContractAddress(blockNumber++, 0);
+    const BNB  = await getContractAddress(blockNumber++, 0);
+    const WXRP = await getContractAddress(blockNumber++, 0);
+    const USDT = await getContractAddress(blockNumber++, 0);
+
     const tokens = { WBTC, WETH, BNB, WXRP, USDT };
+
     const orderbooks: { [pair: string]: string } = {};
+
     for await (const { orderbook, tradedToken, baseToken } of OrderbookCreated.get({ fromBlock: 11, toBlock: 14 })) {
         const pair = `${await ERC20Mock.at(tradedToken).symbol()}/${await ERC20Mock.at(baseToken).symbol()}`;
         orderbooks[pair] = orderbook;
         startOrderbookSimulation(orderbook, verbose);
     }
+
     await server.listen(port);
+
     return { addressBook, operatorFactory, operatorV1, orderbookFactory, tokens, orderbooks };
 }
